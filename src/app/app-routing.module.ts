307d50d1b4eba0629d87b701341ca8166ad85dfa
sourav <souravd2397@gmail.com>
2023-05-20 17:42:22 +0530

    Changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,53 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginnComponent } from './loginn/loginn.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
+import { LaptopComponent } from './products/laptop/laptop.component';
+import { TvComponent } from './products/tv/tv.component';
+import { MobileComponent } from './products/mobile/mobile.component';
+import { WashingmachineComponent } from './products/washingmachine/washingmachine.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'loginn',
+    pathMatch: 'full'
+  },
+  { 
+    path: 'loginn', component: LoginnComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  {
+    path: 'products',
+    // component: ContactComponent,
+    children: [
+      { path: '', component: ProductsComponent },
+      { path: 'product', component: ProductsComponent },
+      { path: 'laptop', component: LaptopComponent },
+      { path: 'tv', component: TvComponent },
+      { path: 'mobile', component: MobileComponent },
+      { path: 'washingmachine', component: WashingmachineComponent }
+    ]
+  },
+  {
+    path: 'contact',
+    children: [
+      { path: 'laptop', component: LaptopComponent },
+      { path: 'tv', component: TvComponent },
+      { path: 'mobile', component: MobileComponent },
+      { path: 'washingmachine', component: WashingmachineComponent }
+    ]
+  },
+  { path: '**', component: NotfoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,39 +18,6 @@ import { TvComponent } from './products/tv/tv.component';
 import { WashingmachineComponent } from './products/washingmachine/washingmachine.component';
 import { CardComponent } from './card/card.component';
 
-const appRoutes: Routes = [
-  {
-    path: '',
-    redirectTo: 'loginn',
-    pathMatch: 'full'
-  },
-  { path: 'loginn', component: LoginnComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
-  {
-    path: 'products',
-    children: [
-      { path: '', component: ProductsComponent },
-      { path: 'product', component: ProductsComponent },
-      { path: 'laptop', component: LaptopComponent },
-      { path: 'tv', component: TvComponent },
-      { path: 'mobile', component: MobileComponent },
-      { path: 'washingmachine', component: WashingmachineComponent }
-    ]
-  },
-  {
-    path: 'contact',
-    children: [
-      { path: 'laptop', component: LaptopComponent },
-      { path: 'tv', component: TvComponent },
-      { path: 'mobile', component: MobileComponent },
-      { path: 'washingmachine', component: WashingmachineComponent }
-    ]
-  },
-  { path: '**', component: NotfoundComponent }
-]
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -70,7 +37,6 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule
   ],
   providers: [],
